2f9ef172474f9c5a7a490acf1f18388686f8b3e1
godnoob3103 <135569620+godnoob3103@users.noreply.github.com>
2026-05-01 16:00:46 +0700

    Update build genome index.sh

--- STAR/build genome index.sh
+++ STAR/build genome index.sh
@@ -1,6 +1,6 @@
 STAR --runThreadN 16 \
      --runMode genomeGenerate \
      --genomeDir ~/reference/star_index \
-     --genomeFastaFiles ~/reference/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz \
-     --sjdbGTFfile ~/reference/Homo_sapiens.GRCh38.113.gtf.gz \
+     --genomeFastaFiles ~/reference/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
+     --sjdbGTFfile ~/reference/Homo_sapiens.GRCh38.113.gtf \
      --sjdbOverhang 100
