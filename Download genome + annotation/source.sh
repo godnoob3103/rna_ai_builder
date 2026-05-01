90827418e2507d06084d6a05dc0f1264ddf6b655
godnoob3103 <135569620+godnoob3103@users.noreply.github.com>
2026-05-01 14:42:03 +0700

    Create source.sh

---
+++ Download genome + annotation/source.sh
@@ -0,0 +1,4 @@
+# Download genome
+wget -c https://ftp.ensembl.org/pub/release-113/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
+# Download annotation
+wget -c https://ftp.ensembl.org/pub/release-113/gtf/homo_sapiens/Homo_sapiens.GRCh38.113.gtf.gz
